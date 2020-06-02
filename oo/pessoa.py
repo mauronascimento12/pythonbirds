1624c1589fefe3a14636acb120531cda4def85fc
mauro nascimento <mauro200714@gmail.com>
2020-06-02 17:28:30 -0300

    Atributos de nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=13):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Mauro')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Silva'
+    print(p.nome)
+    print(p.idade)
