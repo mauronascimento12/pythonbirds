94aca59c0f86b4effb4d672239951ee69b85ee53
mauro nascimento <mauro200714@gmail.com>
2020-06-24 18:12:28 -0300

    foi criado o atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=13):
+    def __init__(self, *filhos, nome=None, idade=13):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Mauro')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Silva'
-    print(p.nome)
-    print(p.idade)
+    silva = Pessoa(nome='Silva')
+    mauro = Pessoa(silva, nome='Mauro')
+    print(Pessoa.cumprimentar(mauro))
+    print(id(mauro))
+    print(mauro.cumprimentar())
+    print(mauro.nome)
+    print(mauro.idade)
+    for filhos in mauro.filhos:
+        print(filhos.nome)
